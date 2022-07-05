fd8d21bea640d912bbbe6d2af895f672453fd3d7
huytu <huytuph@gmail.com>
2022-07-05 23:58:05 +1000

    update

--- Resume_Huy_Tu.py
+++ Resume_Huy_Tu.py
@@ -1,17 +1,23 @@
 import menu_loop, random, time
 
-# 3 second countdown timer before the program starts
-print("starting in..")
-start_countdown = 3
-while start_countdown != 0:
-    print(start_countdown)
-    start_countdown -= 1
-    time.sleep(1)
+myname = [
+" --------------------------------------------------------------",
+"I  _   _         _   _   _____          ____  _                I",
+"I | |_| | _   _ | |_| | |_   _| _   _  |  . || |_  ___  _____  I",
+"I |  _  || |_| ||___  |   | |  | |_| | | ___||   |/.  ||     | I",
+"I |_| |_||_____||_____|   |_|  |_____| |_|   |_|_||_|_||_|_|_| I",
+" --------------------------------------------------------------",
+" "
+]
 
-print("=================\n Huy Tu's Resume\n=================")
+for x in myname:
+    print(x)
+    time.sleep(0.1)
 
 # a simple math problem challenge for the users to enter the main menu
 # will randomly generate a new math problem if answered incorrectly
+
+time.sleep(1)
 print("enter the correct answer to view my resume\nYou have 3 chances")
 auth_count = 3
 while auth_count != 0:
